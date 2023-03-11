2f2af899ec953e5a0c690559689795618e5a3a1c
AllaMinasyan <allaminasyan2006@gmail.com>
2023-03-11 11:23:04 +0400

    Cactus

--- app/src/main/java/com/example/myapplication/Home.java
+++ app/src/main/java/com/example/myapplication/Home.java
@@ -3,12 +3,50 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class Home extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+
+    HomeFragment homeFragment=new HomeFragment();
+    SearchFragment searchFragment=new SearchFragment();
+    NotificationFragment notificationFragment=new NotificationFragment();
+    chatFragment chatFragment=new chatFragment();
+    accountFragment accountFragment=new accountFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected( MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+                        return true;
+                    case R.id.search:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, searchFragment).commit();
+                        return true;
+                    case R.id.notification:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, notificationFragment).commit();
+                        return true;
+                    case R.id.chat:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, chatFragment).commit();
+                        return true;
+                    case R.id.account:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, accountFragment).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
@@ -7,12 +8,17 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button button1, infoBtn;
+    Button button1,button3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,23 +32,24 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, login.class));
             }
         });
-        infoBtn = findViewById(R.id.button3);
-        infoBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override    public void onClick(View view) {
                 AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
-                mBuilder.setIcon(android.R.drawable.sym_def_app_icon);
+                mBuilder.setIcon(R.drawable.kaktus);
                 mBuilder.setTitle(R.string.cactus_icon);
                 mBuilder.setMessage(R.string.info);
                 mBuilder.setCancelable(false);
                 mBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-
+                    @Override            public void onClick(DialogInterface dialogInterface, int i) {
+                        dismissKeyboardShortcutsHelper();
                     }
                 });
+                AlertDialog alertDialog = mBuilder.create();
+                alertDialog.show();
             }
         });
+
 
     }
 }
