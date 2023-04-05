9b435b72deccaba33c32e8588cd2c814ffbe2c1b
AllaMinasyan <allaminasyan2006@gmail.com>
2023-04-06 03:15:05 +0400

    added comment Barec

--- app/src/main/java/com/example/myapplication/HomeFragment.java
+++ app/src/main/java/com/example/myapplication/HomeFragment.java
@@ -7,8 +7,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class HomeFragment extends Fragment {
+    Button button4;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
