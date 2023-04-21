ce9cabd3f696ab82a8f6a7b0276880f797d3c046
AllaMinasyan <allaminasyan2006@gmail.com>
2023-04-22 02:14:27 +0400

    added comment Barec

--- app/src/main/java/com/example/myapplication/SignUp.java
+++ app/src/main/java/com/example/myapplication/SignUp.java
@@ -3,12 +3,25 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.myapplication.databinding.ActivityLoginBinding;
+import com.example.myapplication.databinding.ActivitySignUpBinding;
 
 public class SignUp extends AppCompatActivity {
 
+    ActivitySignUpBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.SignUp.setOnClickListener(new View.OnClickListener(){
+           @Override
+           public void onClick(View view){
+               String name = binding.Անուն.gettext().toString();
+           }
+        });
     }
 }
