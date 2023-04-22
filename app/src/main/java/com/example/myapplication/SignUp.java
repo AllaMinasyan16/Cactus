6c854078e198e8a6bc1cf577aa43d0ddfb1c72ed
AllaMinasyan <allaminasyan2006@gmail.com>
2023-04-22 13:17:19 +0400

    Asxatox cragir

--- app/src/main/java/com/example/myapplication/SignUp.java
+++ app/src/main/java/com/example/myapplication/SignUp.java
@@ -1,27 +1,73 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.myapplication.databinding.ActivityLoginBinding;
 import com.example.myapplication.databinding.ActivitySignUpBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class SignUp extends AppCompatActivity {
 
     ActivitySignUpBinding binding;
+
+    ProgressDialog progressDialog;
+
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore firebaseFirestore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivitySignUpBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        firebaseAuth=FirebaseAuth.getInstance();
+        firebaseFirestore=FirebaseFirestore.getInstance();
+
+        progressDialog=new ProgressDialog(this);
 
         binding.SignUp.setOnClickListener(new View.OnClickListener(){
            @Override
            public void onClick(View view){
-               String name = binding.Անուն.gettext().toString();
+               String name = binding.name.getText().toString();
+               String email = binding.editTextTextEmailAddress.getText().toString();
+               String password = binding.editTextNumberPasswordSignUp.getText().toString();
+               String repeatPassword = binding.editTextNumberPasswordRepeat.getText().toString();
+
+               progressDialog.show();
+               firebaseAuth.createUserWithEmailAndPassword(email,password)
+                       .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                           @Override
+                           public void onSuccess(AuthResult authResult) {
+                               startActivity(new Intent(SignUp.this,login.class));
+                               progressDialog.cancel();
+
+                               firebaseFirestore.collection("User")
+                                       .document(FirebaseAuth.getInstance().getUid())
+                                       .set(new UserModel(name,email));
+
+                           }
+                       })
+                       .addOnFailureListener(new OnFailureListener() {
+                           @Override
+                           public void onFailure(@NonNull Exception e) {
+                               Toast.makeText(SignUp.this, e.getMessage(),Toast.LENGTH_SHORT).show();
+                               progressDialog.cancel();
+                           }
+                       });
+
            }
         });
+
     }
 }
--- app/src/main/java/com/example/myapplication/login.java
+++ app/src/main/java/com/example/myapplication/login.java
@@ -1,19 +1,92 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.myapplication.databinding.ActivityLoginBinding;
+import com.example.myapplication.databinding.ActivitySignUpBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class login extends AppCompatActivity {
+    ActivityLoginBinding binding;
+    FirebaseAuth firebaseAuth;
+    ProgressDialog progressDialog;
     Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        firebaseAuth=FirebaseAuth.getInstance();
+        progressDialog=new ProgressDialog(this);
+
+        binding.login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email=binding.editTextTextEmailAddress.getText().toString().trim();
+                String password=binding.editTextNumberPassword.getText().toString().trim();
+                progressDialog.show();
+                firebaseAuth.signInWithEmailAndPassword(email,password)
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                progressDialog.cancel();
+                                Toast.makeText(login.this, "Login Succesfull",Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(login.this,Home.class));
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                progressDialog.cancel();
+                                Toast.makeText(login.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
+
+        binding.resetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email=binding.editTextTextEmailAddress.getText().toString();
+                progressDialog.setTitle("Sending Mail");
+                progressDialog.show();
+                firebaseAuth.sendPasswordResetEmail(email)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                progressDialog.cancel();
+                                Toast.makeText(login.this, "Email Sent", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                progressDialog.cancel();
+                                Toast.makeText(login.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
+
+        binding.goToSignUpActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(login.this,SignUp.class));
+            }
+        });
 
     }
 }
