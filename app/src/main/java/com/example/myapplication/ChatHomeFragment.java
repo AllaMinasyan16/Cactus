ff9ba3393f5f83f992a68cae057b49c5e6f42cf7
AllaMinasyan <allaminasyan2006@gmail.com>
2023-05-06 12:04:58 +0400

    erjank comment

---
+++ app/src/main/java/com/example/myapplication/ChatHomeFragment.java
@@ -0,0 +1,99 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LifecycleOwner;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.firebase.ui.database.FirebaseListOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
+import android.text.format.DateFormat;
+
+public class ChatHomeFragment extends AppCompatActivity {
+
+    private static int SIGN_IN_CODE=1;
+    private FirebaseListAdapter<Message> adapter;
+    private FloatingActionButton sendBtn;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==SIGN_IN_CODE){
+            if(requestCode==RESULT_OK) {
+                displayAllMessages();
+            }
+        }
+        else{
+            finish();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        sendBtn=findViewById(R.id.btnSend);
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText textField = findViewById(R.id.text_layout);
+                if(textField.getText().toString()==""){
+                    return;
+                }
+                FirebaseDatabase.getInstance().getReference().push().setValue(
+                        new Message(
+                                FirebaseAuth.getInstance().getCurrentUser().getEmail(),
+                                textField.getText().toString()
+                        )
+                );
+                textField.setText("");
+            }
+        });
+
+        setContentView(R.layout.activity_chat_home_fragment);
+        //erjankutyun
+        if(FirebaseAuth.getInstance().getCurrentUser()==null){
+            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(), SIGN_IN_CODE);
+        }
+        else{
+            displayAllMessages();
+        }
+    }
+    private void displayAllMessages(){
+        ListView listOfMessages = findViewById(R.id.List_of_messages);
+        Query query = FirebaseDatabase.getInstance().getReference();
+        FirebaseListOptions<Message> options = new FirebaseListOptions.Builder<Message>()
+                .setLayout(R.layout.list_item)
+                .setQuery(query, Message.class)
+                .setLifecycleOwner(this)
+                .build();
+        adapter = new FirebaseListAdapter<Message>(options) {
+            @Override
+            protected void populateView(View v, Message model, int position) {
+                TextView mess_user, mess_time,mess_text;
+                mess_user = v.findViewById(R.id.message_user);
+                mess_time = v.findViewById(R.id.message_time);
+                mess_text = v.findViewById(R.id.message_text);
+
+                mess_user.setText(model.getUserName());
+                mess_text.setText(model.getTextMessage());
+                mess_time.setText(DateFormat.format("dd-mm-yyyy HH:mm:ss", model.getMessageTime()));
+            }
+        };
+    listOfMessages.setAdapter(adapter);
+    }
+}
--- app/src/main/java/com/example/myapplication/HomeFragment.java
+++ app/src/main/java/com/example/myapplication/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,12 +11,21 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 public class HomeFragment extends Fragment {
-    Button button4;
+    Button button6;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        button6= button6.findViewById(R.id.button6);
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeFragment.this.getActivity(), ChatHomeFragment.class));
+            }
+        });
+
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+
 }
