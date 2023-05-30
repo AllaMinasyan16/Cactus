9ed7993ba7b6b0d83f50a430111a99b7f8f5742e
AllaMinasyan <allaminasyan2006@gmail.com>
2023-05-30 13:31:56 +0400

    login comment

--- app/src/main/java/com/example/myapplication/SearchFragment.java
+++ app/src/main/java/com/example/myapplication/SearchFragment.java
@@ -2,18 +2,37 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 public class SearchFragment extends Fragment {
+    /*
+    ListView listView;
+    String[] numbers = {};
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+   public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                            Bundle savedInstanceState) {
+         Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false);
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.listViewSearch);
+        listView = listView.findViewById(R.id.listViewSearch);
     }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+       getMenuInflater().inflate(R.menu.menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.search);
+
+    }*/
 }
--- app/src/main/java/com/example/myapplication/SignUp.java
+++ app/src/main/java/com/example/myapplication/SignUp.java
@@ -11,8 +11,11 @@ import android.widget.Toast;
 
 import com.example.myapplication.databinding.ActivityLoginBinding;
 import com.example.myapplication.databinding.ActivitySignUpBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -38,36 +41,53 @@ public class SignUp extends AppCompatActivity {
 
         binding.SignUp.setOnClickListener(new View.OnClickListener(){
            @Override
-           public void onClick(View view){
+           public void onClick(View view) {
                String name = binding.name.getText().toString();
                String email = binding.editTextTextEmailAddress.getText().toString();
                String password = binding.editTextNumberPasswordSignUp.getText().toString();
                String repeatPassword = binding.editTextNumberPasswordRepeat.getText().toString();
 
                progressDialog.show();
-               firebaseAuth.createUserWithEmailAndPassword(email,password)
+               firebaseAuth.createUserWithEmailAndPassword(email, password)
                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
                            @Override
                            public void onSuccess(AuthResult authResult) {
-                               startActivity(new Intent(SignUp.this,login.class));
+                               startActivity(new Intent(SignUp.this, login.class));
                                progressDialog.cancel();
 
                                firebaseFirestore.collection("User")
                                        .document(FirebaseAuth.getInstance().getUid())
-                                       .set(new UserModel(name,email));
+                                       .set(new UserModel(name, email));
 
                            }
                        })
                        .addOnFailureListener(new OnFailureListener() {
                            @Override
                            public void onFailure(@NonNull Exception e) {
-                               Toast.makeText(SignUp.this, e.getMessage(),Toast.LENGTH_SHORT).show();
+                               Toast.makeText(SignUp.this, e.getMessage(), Toast.LENGTH_SHORT).show();
                                progressDialog.cancel();
                            }
                        });
+               FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+               FirebaseUser user = firebaseAuth.getCurrentUser();
+               View main = findViewById(R.id.SignUp);
 
+               if (user != null) {
+                   user.sendEmailVerification()
+                           .addOnCompleteListener(new OnCompleteListener<Void>() {
+                               @Override
+                               public void onComplete(@NonNull Task<Void> task) {
+                                   if (task.isSuccessful()) {
+                                       Snackbar.make(main, "Письмо для подтверждения отправлено на " + user.getEmail(), Snackbar.LENGTH_SHORT).show();
+                                   } else {
+                                       Snackbar.make(main, "Ошибка отправки письма для подтверждения " + task.getException().getMessage(), Snackbar.LENGTH_LONG).show();
+                                   }
+                               }
+                           });
+
+               }
            }
         });
 
     }
-}
+    }
